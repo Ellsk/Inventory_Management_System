051b38ff8db436018d6525303dca00f5112b819a
Elliot Kumedzro <ebkumedzro@st.ug.edu.gh>
2023-07-18 09:31:11 +0100

    Search bar working

--- src/main/java/com/example/invenotory_management_system/ProductSearchController.java
+++ src/main/java/com/example/invenotory_management_system/ProductSearchController.java
@@ -2,10 +2,13 @@ package com.example.invenotory_management_system;
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.collections.transformation.FilteredList;
+import javafx.collections.transformation.SortedList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
 import javafx.scene.control.cell.PropertyValueFactory;
 
 import java.net.URL;
@@ -36,20 +39,25 @@ public class ProductSearchController implements Initializable {
     @FXML
     private TableColumn<ProductSearchModule, String> descriptionTableColumn;
 
+    @FXML
+    private TextField keywordTextField;
     ObservableList<ProductSearchModule> productSearchModuleObservableList = FXCollections.observableArrayList();
 
-    public void initialize(URL url, ResourceBundle resourceBundle){
+    public void initialize(URL url, ResourceBundle resourceBundle) {
         DatabaseConnection connectNow = new DatabaseConnection();
         Connection connectDB = connectNow.getConnection();
 
         // SQL Query - Execute the database
         String productViewQuery = "SELECT ProductID, Brand, ModelNumber, ModelYear, ProductName, Description FROM products";
 
-        try{
+        try {
             Statement statement = connectDB.createStatement();
             ResultSet queryOutput = statement.executeQuery(productViewQuery);
 
-            while(queryOutput.next()){
+            // Create filtered list outside the loop
+            FilteredList<ProductSearchModule> filteredData = new FilteredList<>(productSearchModuleObservableList, b -> true);
+
+            while (queryOutput.next()) {
                 Integer queryProductID = queryOutput.getInt("ProductID");
                 String queryBrand = queryOutput.getString("Brand");
                 String queryModelNumber = queryOutput.getString("ModelNumber");
@@ -72,8 +80,36 @@ public class ProductSearchController implements Initializable {
             // Set the observable list to the table view
             productTableView.setItems(productSearchModuleObservableList);
 
+            // Here is the filtered list
+            keywordTextField.textProperty().addListener((observable, oldValue, newValue) -> filteredData.setPredicate(productSearchModule -> {
+                if (newValue.isEmpty() || newValue.isBlank() || newValue == null) {
+                    return true;
+                }
+
+                String searchKeyword = newValue.toLowerCase();
+
+                if (productSearchModule.getProductName().toLowerCase().contains(searchKeyword)) {
+                    return true; // Then we found a productName
+                } else if (productSearchModule.getDescription().toLowerCase().contains(searchKeyword)) {
+                    return true; // Found keyword in description
+                } else // No match
+                    if (productSearchModule.getModelNumber().contains(searchKeyword)) {
+                        return true; // Found keyword in the modelNum
+                    } else return productSearchModule.getModelYear().toString().contains(searchKeyword);
+            }));
+
+            SortedList<ProductSearchModule> sortedData = new SortedList<>(filteredData);
+
+            // For binding sorted result with table
+            sortedData.comparatorProperty().bind(productTableView.comparatorProperty());
+
+            // Apply filtered and sorted data in Table
+            productTableView.setItems(sortedData);
+
+
         } catch (SQLException e) {
             Logger.getLogger(ProductSearchController.class.getName()).log(Level.SEVERE, null, e);
         }
     }
+
 }
